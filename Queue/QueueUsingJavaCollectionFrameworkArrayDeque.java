449e682588b6f1bd1751aedd590ac04bf9ed06fb
chanjosh <chandresh.joshi@oracle.com>
2023-05-12 12:21:53 +0530

    Queue

---
+++ Queue/QueueUsingJavaCollectionFrameworkArrayDeque.java
@@ -0,0 +1,28 @@
+package DS-with-Java.Queue;
+
+//queue using Java Collection Framework
+import java.util.*;
+
+public class QueueUsingJavaCollectionFrameworkArrayDeque {
+    public static void main(String args[]) {
+        // Queue<Integer> q = new LinkedList();
+
+        // here we're creating a Queue to hold integer values. So used: Queue<Integer>
+        // Queue is an Interface in Java, but not a class.
+        // Object can be of Class only but not of Interface.
+        // Queus Interface is implemented by 2 classes as follows-
+        // 1. ArrayDeque, 2. LinkedList
+
+        Queue<Integer> q = new ArrayDeque(); // Double ended queue
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+
+        while (!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
